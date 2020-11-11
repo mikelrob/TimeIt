298f5fd2a4ac111a8ad4031e41e22693acb083d7
Mike Robinson <mike.robinson@slicelife.com>
2020-11-11 12:28:55 +0000

    Adds func to access logs with out removeing them

--- Tests/TimeItTests/TimeItTests.swift
+++ Tests/TimeItTests/TimeItTests.swift
@@ -3,7 +3,27 @@ import TimeIt
 
 final class TimeItTests: XCTestCase {
 
-    func testTimeIt() {
+    func testTimeItAccesses() {
+        TimeIt.log("test", entry: "first")
+        TimeIt.log("test", entry: "second")
+
+        let e = expectation(description: "logs in correct order")
+
+        TimeIt.access(log: "test") { logs in
+            guard logs[0].contains("first") else { XCTFail("first log message not correct"); return }
+            guard logs[1].contains("second") else { XCTFail("second log message not correct"); return }
+        }
+
+        TimeIt.log("test", entry: "third")
+
+        TimeIt.access(log: "test") { logs in
+            guard logs[2].contains("third") else { XCTFail("third log message not correct"); return }
+            e.fulfill()
+        }
+        waitForExpectations(timeout: 0.5)
+    }
+
+    func testTimeItCompletes() {
         TimeIt.log("test", entry: "first")
         TimeIt.log("test", entry: "second")
 
@@ -12,13 +32,13 @@ final class TimeItTests: XCTestCase {
         TimeIt.complete(log: "test") { logs in
             guard logs[0].contains("first") else { XCTFail("first log message not correct"); return }
             guard logs[1].contains("second") else { XCTFail("second log message not correct"); return }
-            e.fulfill()
         }
 
+        TimeIt.log("test", entry: "second")
+
+        TimeIt.complete(log: "test") { logs in
+            guard logs[0].contains("third") else { e.fulfill(); return }
+        }
         waitForExpectations(timeout: 0.5)
     }
-
-    static var allTests = [
-        ("testExample", testTimeIt),
-    ]
 }
